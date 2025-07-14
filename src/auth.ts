cb1f5324f9609b57316ba847a52b41a102eb47af
kishi_masahiro <ksmshr1988@gmail.com>
2025-07-14 23:47:03 +0900

    不要パッケージ削除

--- src/auth.ts
+++ src/auth.ts
@@ -2,9 +2,6 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import type { NextAuthOptions } from "next-auth";
 import axios from "axios";
 
-import type { Session, User } from "next-auth";
-import type { JWT } from "next-auth/jwt";
-
 declare module "next-auth" {
   interface Session {
     user: {
